bbf6d8f5c1b21502a247bf4922d66e732c57e06b
Marzieh Rezaei <marziehrezaei930@gmail.com>
2023-10-13 09:39:56 +0330

    Add project files.

---
+++ Messages/Messages.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    public class Messages
+    {
+        public Messages (string sms, string email, string system)
+        {
+            Sms = sms;
+            Email = email;
+            System = system;
+        }
+        public string Sms {  get; set; }
+        public string Email {  get; set; }
+        public string System {  get; set; }
+    }
+}
---
+++ Messages/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your sms");
+            string sms = Console.ReadLine();
+            Console.WriteLine("Please enter your email address");
+            string email= Console.ReadLine();
+            Console.WriteLine("Please enter your system message");
+            string system= Console.ReadLine();
+            Messages objMessage = new Messages(sms, email, system);
+        }
+    }
+}
